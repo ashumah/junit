0e5ac4851ac70136431153b548e2145e554bed75
Your Name <you@example.com>
2022-03-04 18:03:17 +0530

    commited

---
+++ junit3/src/main/java/junit3/junitDemo.java
@@ -0,0 +1,20 @@
+package junit3;
+
+import org.junit.jupiter.api.Test;
+
+public class junitDemo {
+
+
+		@Test
+		void display1() {
+			System.out.println("welcome to class");
+			System.out.println("hello");
+
+		}
+		
+
+		}
+
+	
+
+
